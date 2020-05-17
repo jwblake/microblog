39bfc6ca9bbcce27e18bed55a097e0bf6a438c0d
jwblake <jonawayneblake@gmail.com>
2020-05-17 16:25:47 -0400

    Chapter 12: Dates and Times

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_migrate import Migrate
+from flask_moment import Moment
 from flask_login import LoginManager
 import logging
 from logging.handlers import RotatingFileHandler
@@ -17,6 +18,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 if not app.debug:
     if not os.path.exists('logs'):
