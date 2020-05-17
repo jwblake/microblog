8b77f6f5165129363fcbac5be1b04d77de2e107e
jwblake <jonawayneblake@gmail.com>
2020-05-17 17:25:37 -0400

    Chapter 13: I18n and L10n

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
@@ -6,6 +6,8 @@ from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_login import LoginManager
+from flask_babel import Babel
+from flask_babel import Babel, lazy_gettext as _l
 import logging
 from logging.handlers import RotatingFileHandler
 import os
@@ -16,9 +18,15 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 if not app.debug:
     if not os.path.exists('logs'):
